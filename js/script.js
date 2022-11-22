2c7004c6494cd0fe3e0bd6c6563a45f60e68aaf4
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2022-11-22 22:25:09 +0000

    change in script.js and added numeric text-field!

--- js/script.js
+++ js/script.js
@@ -19,5 +19,5 @@ if (navigator.serviceWorker) {
  * This function displays an alert.
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  document.getElementById("movie-desiding-button").innerHTML = "<p>Hello, World!</p>"
 }
