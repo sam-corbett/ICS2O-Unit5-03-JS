c8032e66028292080bcc54a00254c4062844d7b2
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2022-11-22 22:05:29 +0000

    added svg picture and edited HTML MDL button

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-03-JS/sw.js", {
+    scope: "/ICS2O-Unit5-03-JS/",
   })
 }
 
