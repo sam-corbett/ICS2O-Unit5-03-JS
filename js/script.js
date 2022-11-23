1be18516a7525618fe1ec539f12355266f397a94
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2022-11-23 18:56:51 +0000

    fixed if, then else if, else statements in script.js

--- js/script.js
+++ js/script.js
@@ -16,13 +16,23 @@ if (navigator.serviceWorker) {
 }
 
 
-// This function helps some dumb thing out
+/**
+ * This function tells you what movie to watch, depending on the age.
+ */
 function myButtonClicked() {
- //input
- var valueOfAge = document.getElementById("ageValue").checked
+ /**
+ * This is the age user enters.
+ */
+ var age = parseFloat(document.getElementById("ageValue").value)
 
  //process
-if (valueOfAge >= 17) {
-   document.getElementById("movie-desiding-button").innerHTML = "You can watch R-Rated Movies"
+  if (age >= 17) {
+    document.getElementById("movie-desiding-button").innerHTML = "<p>You can watch up to R-Rated Movies</p>"
+  } else if (age >= 13) {
+    document.getElementById("movie-desiding-button").innerHTML = "<p>You can only watch up to PG-13 Rated Movies</p>"
+  } else if (age >= 5) {
+    document.getElementById("movie-desiding-button").innerHTML = "<p>You can only watch up to G & PG Rated Movies</p>"
+  } else {
+    document.getElementById("movie-desiding-button").innerHTML = "<p>You are either too young to watch any movie, or not born yet.</p>"
+  }
 }
-
